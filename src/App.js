7546b9b816dc2aca87b16e675c6a3901165b70fe
harshal34480 <harshal34480@gmail.com>
2023-11-18 21:10:23 +0530

    add about secton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="bg-[#0a192f]">
       <Navbar></Navbar>
       <Home></Home>
+      <About></About>
     </div>
   );
 }
